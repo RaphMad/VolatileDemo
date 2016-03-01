0aff65af9943350eecb978d61a5073a000174f0d
MADER Raphael <Raphael.MADER@frequentis.com>
2016-03-01 10:03:17 +0100

    Added source files

---
+++ VolatileDemo/BoolHolder.cs
@@ -0,0 +1,19 @@
+﻿#region header
+
+/*******************************************************
+ *                                                     *
+ * COPYRIGHT: FREQUENTIS AG. All rights reserved.      *
+ *            Registered with Commercial Court Vienna, *
+ *            reg.no. FN 72.115b.                      *
+ *                                                     *
+ *******************************************************/
+
+#endregion
+
+namespace VolatileDemo
+{
+   public class BoolHolder
+   {
+      public bool _isOn;
+   }
+}
---
+++ VolatileDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace VolatileDemo
+{
+   using System;
+   using System.Threading;
+
+   class Program
+   {
+      static void Main()
+      {
+         // var loop = new BoolHolder { _isOn = true };
+         bool loop = true;
+
+         new Thread(() =>
+                    {
+                       Console.WriteLine("Loop started...");
+                       while (loop){}
+                       Console.WriteLine("Loop stopped...");
+                    }).Start();
+
+         Console.ReadKey();
+         loop = false;
+         Console.WriteLine("Flag is now FALSE");
+
+         Console.ReadKey();
+      }
+   }
+}
